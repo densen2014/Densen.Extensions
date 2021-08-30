0283d566fb3a3a5c830c8c06581419d001504509
Alex Chow <zhouchuanglin@gmail.com>
2021-08-30 18:07:35 +0200

    add 数字转字母列标

---
+++ AME.Extensions/Utils/ColsName.cs
@@ -0,0 +1,32 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AME.Util
+{
+    public static class ColsName
+    {
+
+        //C# Excel数字转字母列标
+        public static string 数字转字母列标(int index)
+        {
+            var dividend = index;
+            var columnName = string.Empty;
+
+            while (dividend > 0)
+            {
+                var modulo = (dividend - 1) % 26;
+                columnName = Convert.ToChar(65 + modulo) + columnName;
+                dividend = (dividend - modulo) / 26;
+            }
+
+            return columnName;
+        }
+    }
+}
