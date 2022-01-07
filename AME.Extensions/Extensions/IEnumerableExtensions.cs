48d259cda7aa4a745db38f3c9f26465020f2b3e2
Alex Chow <zhouchuanglin@gmail.com>
2022-01-07 15:56:08 +0100

    IEnumerable 扩展 Add 方法

---
+++ AME.Extensions/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.FileSystemGlobbing;
+using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AME
+{
+    /// <summary>
+    /// IEnumerable 扩展方法
+    /// </summary>
+
+
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<T> Add<T>(this IEnumerable<T> e, T value)
+        {
+            foreach (var cur in e)
+            {
+                yield return cur;
+            }
+            yield return value;
+        }
+
+    }
+
+}
