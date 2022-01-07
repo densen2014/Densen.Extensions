f2a04358625c43d0e8d8767fed47c4c6ef09da67
Alex Chow <zhouchuanglin@gmail.com>
2022-01-07 16:09:58 +0100

    Update IEnumerableExtensions.cs

--- AME.Extensions/Extensions/IEnumerableExtensions.cs
+++ AME.Extensions/Extensions/IEnumerableExtensions.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.FileSystemGlobbing.Abstractions;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AME
 {
@@ -13,15 +15,22 @@ namespace AME
 
     public static class IEnumerableExtensions
     {
-        public static IEnumerable<T> Add<T>(this IEnumerable<T> e, T value)
+        public static IEnumerable Append(this IEnumerable first, params object[] second)
         {
-            foreach (var cur in e)
-            {
-                yield return cur;
-            }
-            yield return value;
+            return first.OfType<object>().Concat(second);
         }
-
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> first, params T[] second)
+        {
+            return first.Concat(second);
+        }
+        public static IEnumerable Prepend(this IEnumerable first, params object[] second)
+        {
+            return second.Concat(first.OfType<object>());
+        }
+        public static IEnumerable<T> Prepend<T>(this IEnumerable<T> first, params T[] second)
+        {
+            return second.Concat(first);
+        }
     }
 
 }
