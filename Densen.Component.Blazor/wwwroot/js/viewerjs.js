539cbf315833fa831a46dbc809bc7add82f80156
Alex Chow <zhouchuanglin@gmail.com>
2022-02-14 13:48:31 +0100

    Viewerjs 改为内置js/css资源, 页面关闭destroy组件

--- Densen.Component.Blazor/wwwroot/js/viewerjs.js
+++ Densen.Component.Blazor/wwwroot/js/viewerjs.js
@@ -1,17 +1,20 @@
-﻿import 'https://cdnjs.cloudflare.com/ajax/libs/viewerjs/1.10.3/viewer.min.js';
+﻿import '/_content/Densen.Component.Blazor/js/viewer.min.js';
 var viewer = null;
 export function initOptions(options) {
     options.title = function (image) {
         return image.alt + ' (' + (this.index + 1) + '/' + this.length + ')';
     };
+    //options.hidden= function () {
+    //    viewer.destroy();
+    //};
     if (undefined !== options.toolbarlite && options.toolbarlite == true) {
         options.toolbar = {
             zoomIn: true,
             zoomOut: true,
             //rotateLeft: true,
             rotateRight: true,
-        //    prev: true,
-        //    next: true,
+            //prev: true,
+            //next: true,
         };
     }
     if (undefined !== viewer && null !== viewer && options.id == viewer.element.id) {
@@ -20,4 +23,10 @@ export function initOptions(options) {
     }
     viewer = new Viewer(document.getElementById(options.id), options);
     console.log(viewer.element.id);
+}
+export function destroy(options) {
+    if (undefined !== viewer && null !== viewer && options.id == viewer.element.id) {
+        viewer.destroy();
+        console.log(viewer.element.id, 'destroy');
+    }
 }
