ee891ea41b32e8990c14d1be051246f0a6d9c085
Alex Chow <zhouchuanglin@gmail.com>
2022-03-06 23:49:35 +0100

    添加GetBattery获取电量和GetNetworkInfo获取网络信息

---
+++ Demo/DemoShared/Pages/WebApiPage.razor.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Argo Zhang (argo@163.com). All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+// Website: https://www.blazor.zone or https://argozhang.github.io/
+
+using AmeBlazor.Components;
+
+namespace DemoShared.Pages;
+
+/// <summary>
+/// WebApi 浏览器api
+/// </summary>
+public sealed partial class WebApiPage
+{
+
+    private List<BatteryStatus> batteryStatus { get; set; } = new List<BatteryStatus>() { new BatteryStatus() };
+    private List<NetworkInfoStatus> networkInfoStatus { get; set; } = new List<NetworkInfoStatus>() { new NetworkInfoStatus() };
+
+    private Task OnBatteryResult(BatteryStatus item)
+    {
+        batteryStatus[0] = item;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private Task OnNetworkInfoResult(NetworkInfoStatus item)
+    {
+        networkInfoStatus[0] = item;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+     
+
+
+    /// <summary>
+    /// 获得属性方法
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    {
+            new AttributeItem("GetBattery","获取电量",  "-","void"),
+            new AttributeItem("GetNetworkInfo","获取网络信息",  "-","void"),
+            new AttributeItem("OnBatteryResult","获取电量回调",  "-","Func<BatteryStatus, Task>"),
+            new AttributeItem("OnNetworkInfoResult","获取网络信息回调",  "-","Func<NetworkInfoStatus, Task>"),
+    };
+}
---
+++ Densen.Component.Blazor/Components/WebAPI/WebApi.razor.cs
@@ -0,0 +1,93 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Diagnostics.CodeAnalysis;
+
+namespace AmeBlazor.Components;
+
+/// <summary>
+/// WebApi 组件基类
+/// </summary>
+public partial class WebApi : IAsyncDisposable
+{
+    [Inject] IJSRuntime? JS { get; set; }
+    private IJSObjectReference? module;
+    private DotNetObjectReference<WebApi>? InstanceWebApi { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            module = await JS!.InvokeAsync<IJSObjectReference>("import", "./_content/Densen.Component.Blazor/lib/webapi/webapi.js");
+            InstanceWebApi = DotNetObjectReference.Create(this);
+            if (OnBatteryResult != null) await GetBattery();
+            if (OnNetworkInfoResult != null) await GetNetworkInfo();
+        }
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (module is not null)
+        {
+            await module.DisposeAsync();
+        }
+    }
+
+    /// <summary>
+    /// 获取电量
+    /// </summary>
+    public virtual async Task GetBattery()
+    {
+        await module!.InvokeVoidAsync("GetBattery", InstanceWebApi);
+    }
+
+    /// <summary>
+    /// 获得/设置 电池信息回调方法
+    /// </summary>
+    [Parameter]
+    public Func<BatteryStatus, Task>? OnBatteryResult { get; set; }
+
+    /// <summary>
+    /// 获取电池信息完成回调方法
+    /// </summary>
+    /// <param name="batteryStatus"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task GetBatteryResult(BatteryStatus batteryStatus)
+    {
+        if (OnBatteryResult != null) await OnBatteryResult.Invoke(batteryStatus);
+    }
+
+
+    /// <summary>
+    /// 获取网络信息
+    /// </summary>
+    public virtual async Task GetNetworkInfo()
+    {
+        await module!.InvokeVoidAsync("GetNetworkInfo", InstanceWebApi);
+    }
+
+    /// <summary>
+    /// 获得/设置 网络信息回调方法
+    /// </summary>
+    [Parameter]
+    public Func<NetworkInfoStatus, Task>? OnNetworkInfoResult { get; set; }
+
+    /// <summary>
+    /// 获取网络信息完成回调方法
+    /// </summary>
+    /// <param name="NetworkInfoStatus"></param>
+    /// <returns></returns>
+    [JSInvokable]
+    public async Task GetNetworkInfoResult(NetworkInfoStatus networkInfoStatus)
+    {
+        if (OnNetworkInfoResult != null) await OnNetworkInfoResult.Invoke(networkInfoStatus);
+    }
+
+
+}
