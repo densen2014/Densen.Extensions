c546c401064b4bead3e7d158ee990e3fb97bb430
Alex Chow <zhouchuanglin@gmail.com>
2022-03-07 02:06:09 +0100

    Update Program.cs
    Update Program.cs

--- Demo/DemoPWA/Program.cs
+++ Demo/DemoPWA/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DemoShared;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Globalization;
 
@@ -8,6 +9,7 @@ CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddDensenExtensions();
