92dad5f2b5dfad8b8c4ec13d51c7c0d279e2648d
Alex Chow <zhouchuanglin@gmail.com>
2022-03-31 18:12:32 +0200

    Record

---
+++ Densen.Component.Blazor/wwwroot/lib/timecatjs/app.js
@@ -0,0 +1,16 @@
+﻿import '/_content/Densen.Component.Blazor/lib/timecatjs/timecat.global.prod.js';
+
+export function init(wrapperc, element, alertText, backgroundColor) {
+    const recorder = new Recorder();
+    const player = new Player();
+    // receive data on callback
+    recorder.onData(data)
+    console.log('ss')
+    recorder.next()
+
+    // stop record and clean all watchers
+    recorder.destroy()
+
+    // clear all records in db
+    recorder.clearDB()
+}
