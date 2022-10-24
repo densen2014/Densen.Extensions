0e583599e372c35c739e0514cfc8861bf9ce4069
Alex Chow <zhouchuanglin@gmail.com>
2022-10-24 13:44:27 +0200

    光学字符识别 OCR / AI表格识别 AI Form 调优

---
+++ Demo/DemoShared/Pages/AiFormPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
+using BootstrapBlazor.Ocr.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.JSInterop;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+
+namespace DemoShared.Pages
+{
+    public partial class AiFormPage
+    {
+        List<AnalyzedDocument> models { get; set; }
+        private Task OnResult(List<AnalyzedDocument> models)
+        {
+            this.models = models;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Demo/DemoShared/Pages/OcrPage.razor.cs
@@ -0,0 +1,23 @@
+﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
+using BootstrapBlazor.Ocr.Services;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using Microsoft.JSInterop;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+
+
+namespace DemoShared.Pages
+{
+    public partial class OcrPage
+    {
+        List<string> res { get; set; }
+        private Task OnResult(List<string> res)
+        {
+            this.res = res;
+            StateHasChanged();
+            return Task.CompletedTask;
+        }
+    }
+}
