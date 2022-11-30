40d922bd1dc2de480ad2ad642667e36a0041ca77
Alex Chow <zhouchuanglin@gmail.com>
2022-11-30 19:00:38 +0100

    添加 maui HardwareTools 工程

---
+++ Mauis/HardwareTools/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HardwareTools
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Mauis/HardwareTools/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace HardwareTools
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
