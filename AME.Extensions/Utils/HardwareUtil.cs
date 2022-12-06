bff814a062dca4a7dc704043f136631d76084755
Alex Chow <zhouchuanglin@gmail.com>
2022-12-06 15:37:30 +0100

    Update HardwareUtil.cs

--- AME.Extensions/Utils/HardwareUtil.cs
+++ AME.Extensions/Utils/HardwareUtil.cs
@@ -15,7 +15,7 @@ namespace AME.Util
     {
 
 #if NET48 || WINDOWS10_0_19041_0_OR_GREATER
-   public static string GetFirstPhysicalMediaID()
+        public static string GetFirstPhysicalMediaID()
         {
             return GetPhysicalMediaID().FirstOrDefault();
         }
@@ -54,15 +54,74 @@ namespace AME.Util
             }
             return ids;
         }
+        public class DiskDriveID
+        {
+            public string MediaType { get; set; }
+            public string Model { get; set; }
+            public string SerialNumber { get; set; }
+            public string InterfaceType { get; set; }
+            public string Size { get; set; }
+        }
+        public static List<DiskDriveID> GetDiskDriveIDS(bool orderByInterfaceType=true)
+        {
+
+            var ids = new List<DiskDriveID>();
+            var keys = new List<string>() {
+                "MediaType",
+                "Model",
+                "SerialNumber",
+                "InterfaceType",
+            };
+            try
+            {
+                var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+                var id = new DiskDriveID();
+                foreach (var share in searcher.Get())
+                {
+                    id = new DiskDriveID();
+                    foreach (var key in keys)
+                    {
+                        if (share[key] != null && !string.IsNullOrEmpty(share[key].ToString()))
+                        {
+                            var value = share[key].ToString().Trim();
+                            switch (key)
+                            {
+                                case "MediaType":
+                                    id.MediaType = value;
+                                    break;
+                                case "Model":
+                                    id.Model = value;
+                                    break;
+                                case "SerialNumber":
+                                    id.SerialNumber = value;
+                                    break;
+                                case "InterfaceType":
+                                    id.InterfaceType = value;
+                                    break;
+                                case "Size":
+                                    id.Size = ((Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024) < 1 ? (Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024) + " MB") : (Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024) + " GB"));
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                    }
+                    ids.Add(id);
+                }
+            }
+            catch
+            {
+            }
+            return orderByInterfaceType?ids.OrderBy(a => a.InterfaceType).ToList(): ids;
+        }
 
         public static List<string> GetDiskDriveID()
-        {
-             
+        { 
             var ids = new List<string>();
-            var keys = new List<string>() { 
-                "MediaType", 
-                "Model", 
-                "SerialNumber", 
+            var keys = new List<string>() {
+                "MediaType",
+                "Model",
+                "SerialNumber",
                 "InterfaceType", 
                 //"Partitions", 
                 //"Signature", 
@@ -78,9 +137,9 @@ namespace AME.Util
                 var id = "";
                 foreach (var share in searcher.Get())
                 {
-                    keys.ForEach (a=> id += a + " : "+ share[a]?.ToString() + System.Environment.NewLine);
-                    id += "Size" + " : " + ((Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024)<1?(Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024) + " MB"): (Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024) + " GB"));
-                    ids.Add (id);
+                    keys.ForEach(a => id += a + " : " + share[a]?.ToString() + System.Environment.NewLine);
+                    id += "Size" + " : " + ((Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024) < 1 ? (Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024) + " MB") : (Math.Round(Convert.ToDouble(share["Size"]) / 1024 / 1024 / 1024) + " GB"));
+                    ids.Add(id);
                     id = "";
                 }
             }
