2de6529191800c6b323ba0e9b5d7823738e6ee24
Alex chow <zhouchuanglin@gmail.com>
2022-12-26 22:04:20 +0100

    BootstrapBlazor.PdfReader 7.1.4
    BootstrapBlazor.PdfReader 7.1.4
    https://www.nuget.org/packages/BootstrapBlazor.PdfReader/#readme-body-tab
    
    添加
    1. NavigateToPage(int page)
    2 .Refresh(string? search, int? page, EnumPageMode? pagemode, EnumZoomMode? zoom)
    3. Pagemode 页面模式, EnumPageMode 类型
    4. Zoom 缩放模式, EnumZoomMode 类型

---
+++ Demo/DemoShared/Pages/PdfReaders.razor.cs
@@ -0,0 +1,84 @@
+﻿using BootstrapBlazor.Components;
+using System.ComponentModel;
+
+namespace DemoShared.Pages;
+
+public sealed partial class PdfReaders
+{
+    private EnumZoomMode Zoom { get; set; } = EnumZoomMode.Auto;
+
+    private EnumPageMode Pagemode { get; set; } = EnumPageMode.Thumbs;
+
+    [DisplayName("搜索")]
+    private string Search { get; set; } = "Performance";
+
+    private int Page { get; set; } = 3;
+
+    PdfReader pdfReader { get; set; }
+
+    PdfReader pdfReader2 { get; set; }
+
+    [DisplayName("文件相对路径或者URL")]
+    private string Filename { get; set; } = "/_content/DemoShared/samples/sample.pdf";
+
+    private string FilenameStream { get; set; } = "https://localhost:5011/_content/DemoShared/samples/sample.pdf";
+
+    [DisplayName("流模式")]
+    private bool StreamMode { get; set; }
+
+    private bool Debug { get; set; }
+
+    private async Task Apply()
+    {
+        await pdfReader.Refresh();
+    }
+
+    private async Task ApplyZoom()
+    {
+        Zoom = Zoom switch
+        {
+            EnumZoomMode.Auto => EnumZoomMode.PageFit,
+            EnumZoomMode.PageFit => EnumZoomMode.PageWidth,
+            EnumZoomMode.PageWidth => EnumZoomMode.PageHeight,
+            EnumZoomMode.PageHeight => EnumZoomMode.Zoom75,
+            EnumZoomMode.Zoom75 => EnumZoomMode.Zoom50,
+            EnumZoomMode.Zoom50 => EnumZoomMode.Zoom25,
+            _ => EnumZoomMode.Auto
+        };
+        await pdfReader2?.Refresh();
+    }
+
+    private async Task ApplyPagemode()
+    {
+        Pagemode = Pagemode switch
+        {
+            EnumPageMode.Thumbs => EnumPageMode.Outline,
+            EnumPageMode.Outline => EnumPageMode.Attachments,
+            EnumPageMode.Attachments => EnumPageMode.Layers,
+            EnumPageMode.Layers => EnumPageMode.None,
+            _ => EnumPageMode.Thumbs
+        };
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+    }
+    private async Task ApplyPage()
+    {
+        Search = null;
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+    }
+    private async Task ApplyPagePrevious()
+    {
+        Page--;
+        Search = null;
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+    }
+    private async Task ApplyPageNext()
+    {
+        Page++;
+        Search = null;
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+    }
+    private async Task ApplySearch()
+    {
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+    }
+}
