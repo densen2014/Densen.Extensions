226cd8549e7010f685ef63ed006c9fd8d6bb5588
Alex Chow <zhouchuanglin@gmail.com>
2023-01-02 17:43:53 +0100

    禁用复制/打印/下载

--- Demo/DemoShared/Pages/PdfReaders.razor.cs
+++ Demo/DemoShared/Pages/PdfReaders.razor.cs
@@ -1,13 +1,14 @@
 ﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
 using System.ComponentModel;
 
 namespace DemoShared.Pages;
 
 public sealed partial class PdfReaders
 {
-    private EnumZoomMode Zoom { get; set; } = EnumZoomMode.Auto;
+    private EnumZoomMode Zoom { get; set; } = EnumZoomMode.PageHeight;
 
-    private EnumPageMode Pagemode { get; set; } = EnumPageMode.Thumbs;
+    private EnumPageMode Pagemode { get; set; } = EnumPageMode.None;
 
     [DisplayName("搜索")]
     private string Search { get; set; } = "Performance";
@@ -19,7 +20,10 @@ public sealed partial class PdfReaders
     PdfReader pdfReader2 { get; set; }
 
     [DisplayName("文件相对路径或者URL")]
-    private string Filename { get; set; } = "/_content/DemoShared/samples/sample.pdf";
+    private string Filename1 { get; set; } = "/_content/DemoShared/samples/sample.pdf";
+
+    [DisplayName("文件相对路径或者URL")]
+    private string Filename { get; set; } = "/_content/DemoShared/samples/sample2.pdf";
 
     private string FilenameStream { get; set; } = "https://blazor.app1.es/_content/DemoShared/samples/sample.pdf";
 
@@ -27,6 +31,9 @@ public sealed partial class PdfReaders
     private bool StreamMode { get; set; }
 
     private bool Debug { get; set; }
+
+    [DisplayName("禁用复制/打印/下载")]
+    public bool ReadOnly { get; set; } = true;
 
     private async Task Apply()
     {
@@ -58,27 +65,27 @@ public sealed partial class PdfReaders
             EnumPageMode.Layers => EnumPageMode.None,
             _ => EnumPageMode.Thumbs
         };
-        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom, ReadOnly);
     }
     private async Task ApplyPage()
     {
         Search = null;
-        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom, ReadOnly);
     }
     private async Task ApplyPagePrevious()
     {
         Page--;
         Search = null;
-        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom, ReadOnly);
     }
     private async Task ApplyPageNext()
     {
         Page++;
         Search = null;
-        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom, ReadOnly);
     }
     private async Task ApplySearch()
     {
-        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom);
+        await pdfReader2?.Refresh(Search, Page, Pagemode, Zoom, ReadOnly);
     }
 }
