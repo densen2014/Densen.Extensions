0cee1e00eb17804f1dfb3993ce01b2cde25073ae
Alex Chow <zhouchuanglin@gmail.com>
2023-01-24 16:36:59 +0100

    添加 ScreenCapturePage 截屏/录像

--- Demo/DemoShared/Pages/OcrPage.razor.cs
+++ Demo/DemoShared/Pages/OcrPage.razor.cs
@@ -1,14 +1,4 @@
-﻿using Azure.AI.FormRecognizer.DocumentAnalysis;
-using BootstrapBlazor.Ocr.Services;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
-using Microsoft.JSInterop;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-
-
-namespace DemoShared.Pages
+﻿namespace DemoShared.Pages
 {
     public partial class OcrPage
     {
