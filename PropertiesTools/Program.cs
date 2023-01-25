6188c329e02210c0362ffb13bb26e2c452ae293b
Alex Chow <zhouchuanglin@gmail.com>
2023-01-25 16:25:23 +0100

    添加 PdfReader 资源文件批量改后缀名(properties)工具

---
+++ PropertiesTools/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var fileinfo = Directory.GetFiles(@"F:\Repos\BootstrapBlazor.PdfReader\src\BootstrapBlazor.PdfReader\wwwroot\web\locale", "*.properties", SearchOption.AllDirectories);
+foreach (var item in fileinfo)
+{
+    File.Move(item, item.Replace(".properties", ".txt"));
+}
