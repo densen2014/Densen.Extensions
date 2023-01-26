1e1913d6e4251db034d7e0809aa3beda1bb16c86
Alex Chow <zhouchuanglin@gmail.com>
2023-01-26 13:45:42 +0100

    更新 PdfReader 兼容模式例子

--- PropertiesTools/Program.cs
+++ PropertiesTools/Program.cs
@@ -1,7 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-var fileinfo = Directory.GetFiles(@"F:\Repos\BootstrapBlazor.PdfReader\src\BootstrapBlazor.PdfReader\wwwroot\web\locale", "*.properties", SearchOption.AllDirectories);
+var basepath = @"F:\Repos\BootstrapBlazor.PdfReader\src\BootstrapBlazor.PdfReader\wwwroot\compat\web\";
+var fileinfo = Directory.GetFiles(basepath + "locale", "*.properties", SearchOption.AllDirectories);
 foreach (var item in fileinfo)
 {
     File.Move(item, item.Replace(".properties", ".txt"));
 }
+var locale_txt = File.ReadAllText(basepath + @"locale\locale.txt").Replace(".properties", ".txt");
+File.WriteAllText(basepath + @"locale\locale.txt", locale_txt);
+
+locale_txt = File.ReadAllText(basepath + @"viewer.html").Replace("locale.properties", "locale.txt");
+File.WriteAllText(basepath + @"viewer.html", locale_txt);
