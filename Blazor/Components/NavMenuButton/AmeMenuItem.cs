76a96b987fdde9616357448a7766636f84189811
Alex chow <zhouchuanglin@gmail.com>
2023-03-16 12:12:25 +0100

    NavMenuItem 改名 AmeMenu, 支持 SubItems

--- Blazor/Components/NavMenuButton/AmeMenuItem.cs
+++ Blazor/Components/NavMenuButton/AmeMenuItem.cs
@@ -8,11 +8,13 @@ using Microsoft.AspNetCore.Components.Routing;
 
 namespace AmeBlazor.Components
 {
-    public class NavMenuItem
+    public class AmeMenuItem
     {
         public string Link { get; set; }
         public string Text { get; set; }
         public NavLinkMatch Match { get; set; }
         public string AuthorizeRoles { get; set; }
+        public List<AmeMenuItem> SubItems { get; set; }
+
     }
 }
