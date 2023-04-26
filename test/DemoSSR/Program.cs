a34912f7f2a48cde14631dd93779aa84783c29fd
Alex chow <zhouchuanglin@gmail.com>
2023-04-26 16:42:31 +0200

    test bb menu

---
+++ test/DemoSSR/Program.cs
@@ -0,0 +1,141 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.FileProviders;
+using System.Globalization;
+using System.Reflection;
+using System.Text.Encodings.Web; 
+
+string UploadPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "uploads");
+if (!Directory.Exists(UploadPath)) Directory.CreateDirectory(UploadPath);
+
+var builder = WebApplication.CreateBuilder(args); //    webBuilder.UseContentRoot("D:\\T9WMS\\publish");
+
+
+builder.Services.AddCors();
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor(a =>
+{
+    a.DetailedErrors = true;
+    a.JSInteropDefaultCallTimeout = TimeSpan.FromMinutes(2);
+    a.MaxBufferedUnacknowledgedRenderBatches = 20;
+    a.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(10);
+}).AddHubOptions(o =>
+{
+    o.EnableDetailedErrors = true;
+    //单个传入集线器消息的最大大小。默认 32 KB	
+    o.MaximumReceiveMessageSize = null;
+    //可为客户端上载流缓冲的最大项数。 如果达到此限制，则会阻止处理调用，直到服务器处理流项。
+    o.StreamBufferCapacity = 20;
+});
+builder.Services.AddBootstrapBlazor();
+builder.Services.ConfigureJsonLocalizationOptions(op =>
+{
+    // 忽略文化信息丢失日志
+    op.IgnoreLocalizerMissing = true;
+
+    // 附加自己的 json 多语言文化资源文件 如 zh-TW.json
+    op.AdditionalJsonAssemblies = new Assembly[]
+    {
+                //typeof(BootstrapBlazor.Shared.App).Assembly,
+                typeof(BootstrapBlazor.Components.Chart).Assembly,
+                //typeof(BootstrapBlazor.Components.SignaturePad).Assembly
+    };
+});
+
+
+if (!builder.Services.Any(x => x.ServiceType == typeof(HttpClient)))
+{
+    builder.Services.AddSingleton<HttpClient>();
+}
+builder.Services.ConfigureJsonLocalizationOptions(op =>
+{
+    // 附加自己的 json 多语言文化资源文件 如 zh-TW.json
+    op.AdditionalJsonAssemblies = new Assembly[]
+    {
+                typeof(DemoShared.App).Assembly,
+                typeof(BootstrapBlazor.Components.Chart).Assembly,
+    };
+});
+builder.Services.AddControllers();
+
+
+var cultureInfo = new CultureInfo("zh-CN");
+CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+//设置文件上传的大小限制 , 默认值128MB 
+builder.Services.Configure<FormOptions>(o => o.MultipartBodyLengthLimit = long.MaxValue);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+if (string.IsNullOrWhiteSpace(app.Environment.WebRootPath))
+{
+    app.Environment.WebRootPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot");
+}
+var dir = Path.Combine(app.Environment.WebRootPath, "Upload");
+if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+
+var provider = new FileExtensionContentTypeProvider();
+provider.Mappings[".properties"] = "application/octet-stream";
+
+provider.Mappings.Remove(".ts");
+provider.Mappings.Add(".key", "text/plain");
+provider.Mappings.Add(".m3u8", "application/x-mpegURL");
+provider.Mappings.Add(".ts", "video/MP2T");
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    ContentTypeProvider = provider
+});
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(dir),
+    RequestPath = new PathString("/Upload"),
+    ContentTypeProvider = provider
+});
+
+var opt = new DirectoryBrowserOptions
+{
+    FileProvider = new PhysicalFileProvider(dir),
+    Formatter = new AME.HtmlDirectoryFormatterChsSorted(HtmlEncoder.Default),
+    RequestPath = new PathString("/Upload")
+};
+
+app.UseDirectoryBrowser(opt);
+
+app.UseRouting();
+
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin()
+          .AllowAnyMethod()
+          .AllowAnyHeader();
+});
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.MapDefaultControllerRoute();
+app.Run();
---
+++ test/DemoShared/Shared/NavMenu.razor.cs
@@ -0,0 +1,68 @@
+﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace DemoShared.Shared;
+
+//[JSModuleAutoLoader("./_content/DemoShared/modules/menu.js", ModuleName = "Menu", Relative = false)]
+[JSModuleAutoLoader("menu", ModuleName = "Menu")]
+public partial class NavMenu
+{
+    private IEnumerable<MenuItem> Menus { get; set; } = new List<MenuItem>
+    {
+            new MenuItem() { Text = "首页", Url = "/"  , Match = NavLinkMatch.All},
+            new MenuItem() { Text = "工具" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "文件预览 FileViewer", Url = "/fileViewers" },
+                    new MenuItem() { Text = "视频播放器 Video Player", Url = "/videoPlayers" },
+                    new MenuItem() { Text = "PDF阅读器 PDF Reader", Url = "/pdfReaders" },
+                    new MenuItem() { Text = "签名 SignaturePad", Url = "/signaturepad" },
+                    new MenuItem() { Text = "定位 Geolocation", Url = "/geolocations" },
+                    new MenuItem() { Text = "图片浏览 Viewer", Url = "/viewer" },
+                    new MenuItem() { Text = "蓝牙和打印 Bluetooth & Printer", Url = "/Bluetooth" },
+                    new MenuItem() { Text = "文件系统 FileSystem", Url = "/filesystems" },
+                    new MenuItem() { Text = "屏幕键盘 OSK", Url = "/onscreenkeyboards" },
+                    new MenuItem() { Text = "系统信息 System info", Url = "/webapis" },
+                    new MenuItem() { Text = "扫码 Barcode", Url = "/barcodescanner" },
+                    new MenuItem() { Text = "签名 Handwritten", Url = "/handwritten" },
+                    new MenuItem() { Text = "截屏/录像 Screen Capture", Url = "/screencapture" },
+                }
+            },
+            new MenuItem() { Text = "地图" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "百度地图 Baidu Map", Url = "/baidumap" },
+                    new MenuItem() { Text = "谷歌地图 Maps", Url = "/maps" },
+                }
+            },
+            new MenuItem() { Text = "云服务" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "光学字符识别 OCR", Url = "/ocr" },
+                    new MenuItem() { Text = "AI表格识别 AI Form", Url = "/aiform" },
+                    new MenuItem() { Text = "翻译 Translate", Url = "/Translate" },
+                    new MenuItem() { Text = "播放语音/文本转语音 PlayAudio", Url = "/PlayAudio" },
+                    new MenuItem() { Text = "OpenAI", Url = "/OpenAI" },
+                    //new MenuItem() { Text = "AzureOpenAI", Url = "/AzureOpenAI" },
+                }
+            },        
+            new MenuItem() { Text = "图表", Url = "/charts" },
+             new MenuItem() { Text = "文件管理" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "文件", Url = "/Files" },
+                    new MenuItem() { Text = "上传文件", Url = "/FileUpload" },
+                    new MenuItem() { Text = "文件夹", Url = "/AppFiles" },
+                    new MenuItem() { Text = "下载", Url = "/Downloads" },
+                }
+           },
+           new MenuItem() { Text = "其他" , Items = new List<MenuItem>{
+                    new MenuItem() { Text = "TableLazy", Url = "/tablelazy" },
+                    new MenuItem() { Text = "Clock", Url = "/clock" },
+                    new MenuItem() { Text = "IP", Url = "/ip" },
+                } 
+            },
+            new MenuItem() { Text = "测试" ,Items= new List<MenuItem>
+                {
+                    new MenuItem() { Text = "视频墙", Url = "/VideoWall" },
+                    new MenuItem() { Text = "Iframe下载文件", Url = "/TestIframe2" },
+                }
+            },
+    };
+}
