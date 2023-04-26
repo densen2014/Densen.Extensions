68d6b9db89a53763d2a6612db121b2ccfb5d1faf
Alex chow <zhouchuanglin@gmail.com>
2023-04-26 18:07:58 +0200

    NavMenu 组件 依赖新 bb 包 7.5.7

--- Demo/DemoShared/Shared/NavMenu.razor.cs
+++ Demo/DemoShared/Shared/NavMenu.razor.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Components.Routing;
 
 namespace DemoShared.Shared;
 
-//[JSModuleAutoLoader("./_content/DemoShared/modules/menu.js", ModuleName = "Menu", Relative = false)]
-[JSModuleAutoLoader("menu", ModuleName = "Menu")]
 public partial class NavMenu
 {
     private IEnumerable<MenuItem> Menus { get; set; } = new List<MenuItem>
