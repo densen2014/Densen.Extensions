8bcf4a45828ce72584a87ad0c3ae57980cbe0fa0
Alex chow <zhouchuanglin@gmail.com>
2023-06-03 19:00:21 +0200

    Update TableImgField.cs

--- Blazor/Components/TableImgField.cs
+++ Blazor/Components/TableImgField.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BootstrapBlazor.Components;
+using Microsoft.AspNetCore.Components;
 
 namespace AmeBlazor.Components
 {
@@ -19,5 +16,13 @@ namespace AmeBlazor.Components
         public string Name { get; set; }
         public string BaseUrl {  get; set;}
         public string Style { get; set; }
+
+        public EventCallback<object> Callback { get; set; }
+
+        /// <summary>
+        /// 获得/设置 识别完成回调方法,返回 Model 集合
+        /// </summary>
+        public Func<object, Task> CallbackFunc { get; set; }
     }
+     
 }
