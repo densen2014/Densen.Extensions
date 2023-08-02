97bad19a142801fa41d59ce0f140e3f7664c9415
Alex chow <zhouchuanglin@gmail.com>
2023-08-02 15:00:02 +0800

    更新例子

---
+++ Demo/DemoShared/Pages/WebSerialPage.razor.cs
@@ -0,0 +1,146 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+
+using BootstrapBlazor.Components;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DemoShared.Pages;
+
+public partial class WebSerialPage
+{
+
+    private string? message;
+    private string? statusmessage;
+    private string? errmessage;
+    private WebSerialOptions options = new WebSerialOptions() { BaudRate = 115200,BufferSize=102400 };
+
+    [NotNull]
+    private IEnumerable<SelectedItem> BaudRateList { get; set; } = ListToSelectedItem();
+
+    [DisplayName("波特率")]
+    private int SelectedBaudRate { get; set; } = 115200;
+    private bool Flag { get; set; } 
+    private WebSerial? WebSerial { get; set; } 
+
+ 
+    private Task OnReceive(string? message)
+    {
+        this.message += $"{DateTime.Now:hh:mm:ss} 收到数据: {message}{Environment.NewLine}";
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private Task OnLog(string message)
+    {
+        this.statusmessage = message;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    private Task OnError(string message)
+    {
+        this.errmessage = message;
+        StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+    public static IEnumerable<SelectedItem> ListToSelectedItem()
+    {
+        foreach (var item in WebSerialOptions.BaudRateList)
+        {
+            yield return new SelectedItem(item.ToString(), item.ToString());
+        }
+    }
+    private Task OnApply()
+    {
+        options.BaudRate = SelectedBaudRate;
+        Flag = !Flag;
+        //StateHasChanged();
+        return Task.CompletedTask;
+    }
+
+
+    /// <summary>
+    /// 获得属性方法
+    /// </summary>
+    /// <returns></returns>
+    protected IEnumerable<AttributeItem> GetAttributes() => new AttributeItem[]
+    {
+        new AttributeItem() {
+            Name = "OnReceive",
+            Description = "收到数据回调方法",
+            Type = "Func<string, Task>?",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem() {
+            Name = "OnLog",
+            Description = "Log回调方法",
+            Type = "Func<string, Task>?",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem() {
+            Name = "OnError",
+            Description = "错误回调方法",
+            Type = "Func<string, Task>?",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem() {
+            Name = "Element",
+            Description = "UI界面元素的引用对象,为空则使用整个页面",
+            Type = "ElementReference",
+            ValueList = "-",
+            DefaultValue = "-"
+        },
+        new AttributeItem() {
+            Name = "ConnectBtnTitle",
+            Description = "获得/设置 连接按钮文本",
+            Type = "string",
+            ValueList = "",
+            DefaultValue = "连接"
+        },
+        new AttributeItem() {
+            Name = "WriteBtnTitle",
+            Description = "获得/设置 写入按钮文本",
+            Type = "string",
+            ValueList = "",
+            DefaultValue = "写入"
+        },
+        new AttributeItem() {
+            Name = "ShowUI",
+            Description = "获得/设置 显示内置UI",
+            Type = "bool",
+            ValueList = "True|False",
+            DefaultValue = "False"
+        },
+        new AttributeItem() {
+            Name = "Debug",
+            Description = "获得/设置 显示log",
+            Type = "bool",
+            ValueList = "True|False",
+            DefaultValue = "False"
+        },
+        };
+
+
+    /// <summary>s
+    /// 获得WebSerialOptions属性方法
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerable<AttributeItem> GetWebSerialOptionsAttributes() => new AttributeItem[]
+    {
+            new AttributeItem(nameof(WebSerialOptions.BaudRate),"波特率",  "9600","int"),
+            new AttributeItem(nameof(WebSerialOptions.DataBits),"数据位",  "8","int","7|8"),
+            new AttributeItem(nameof(WebSerialOptions.ParityType),"流控制",  "none",nameof(WebSerialFlowControlType),"none|even|odd"),
+            new AttributeItem(nameof(WebSerialOptions.StopBits),"停止位",  "1","int","1|2"),
+            new AttributeItem(nameof(WebSerialOptions.BufferSize),"读写缓冲区",  "255","int"),
+            new AttributeItem(nameof(WebSerialOptions.FlowControlType),"校验",  "none",nameof(WebSerialParityType),"none|hardware"),
+        };
+}
