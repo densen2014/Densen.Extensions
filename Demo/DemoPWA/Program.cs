e6b40933645644f26d08af111f8ae9aee9da3288
Alex chow <zhouchuanglin@gmail.com>
2023-12-14 16:35:30 +0100

    DemoPWA 更新

--- Demo/DemoPWA/Program.cs
+++ Demo/DemoPWA/Program.cs
@@ -20,6 +20,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddDensenExtensions();
+builder.Services.AddStorages();
 builder.Services.ConfigureJsonLocalizationOptions(op =>
 {
     // 忽略文化信息丢失日志
