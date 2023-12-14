49607f0b07155dff61a06ff04b03c7e05bb5823a
Alex chow <zhouchuanglin@gmail.com>
2023-12-14 15:23:24 +0100

    添加组件 BootstrapBlazor.ImageHelper 演示, 基于OpenCV

--- Demo/DemoShared/Shared/NavMenu.razor.cs
+++ Demo/DemoShared/Shared/NavMenu.razor.cs
@@ -76,7 +76,7 @@ public partial class NavMenu
                     new MenuItem() { Text = "testocr", Url = "/testocr" },
                     new MenuItem() { Text = "菜哥AI", Url = "/testopenai" },
                     new MenuItem() { Text = "扫码API", Url = "/testcodes" },
-                    new MenuItem() { Text = "图片裁剪", Url = "/testcanvas" },
+                    new MenuItem() { Text = "OpenV", Url = "/testcanvas" },
                 }
             },
             new MenuItem() { Text = "地图" ,Items= new List<MenuItem>
