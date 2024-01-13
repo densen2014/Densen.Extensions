bbad8551a28b0c28e1c89791614915e14f092314
Alex chow <zhouchuanglin@gmail.com>
2024-01-13 01:06:04 +0100

    Update EnumsGeneral.cs

--- Blazor/Enums/EnumsGeneral.cs
+++ Blazor/Enums/EnumsGeneral.cs
@@ -4,29 +4,28 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
-namespace AME
+namespace AME;
+
+public partial class EnumsExtensions
 {
-    public partial class EnumsExtensions
+
+    public enum ShowToolbarType
     {
+        无,
+        导入Excel,
+        执行添加,
+        导出,
+        升级,
+        刷新,
+        打印,
+    }
+    public enum ShowDetailRowType
+    {
+        表内明细,
+        弹窗,
+        图片列,
+        选项卡,
+        表内明细II,
 
-        public enum ShowToolbarType
-        {
-            无,
-            导入Excel,
-            执行添加,
-            导出,
-            升级,
-            刷新,
-            打印,
-        }
-        public enum ShowDetailRowType
-        {
-            表内明细,
-            弹窗,
-            图片列,
-            选项卡,
-            表内明细II,
-
-        }
     }
 }
