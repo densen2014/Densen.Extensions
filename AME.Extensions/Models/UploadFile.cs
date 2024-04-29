3c629b14bd7203cc73f42a424b0ecad5aa5682c1
Alex chow <zhouchuanglin@gmail.com>
2024-04-29 13:58:43 +0200

    Update UploadFile.cs

--- AME.Extensions/Models/UploadFile.cs
+++ AME.Extensions/Models/UploadFile.cs
@@ -4,36 +4,34 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
-namespace AME.Models
+namespace AME.Models;
+
+//     上传组件返回类
+public class UploadFile
 {
-    //     上传组件返回类
-    public class UploadFile
-    {
 
-        //
-        // 摘要:
-        //     获得/设置 文件名
-        public string FileName { get; set; }
-        //
-        // 摘要:
-        //     获得/设置 原始文件名
-        public string OriginFileName { get; }
-        //
-        // 摘要:
-        //     获得/设置 文件大小
-        public long Size { get; set; }
-        //
-        // 摘要:
-        //     获得/设置 文件上传结果 0 表示成功 非零表示失败
-        public int Code { get; set; }
-        //
-        // 摘要:
-        //     获得/设置 文件预览地址
-        public string PrevUrl { get; set; }
-        //
-        // 摘要:
-        //     获得/设置 错误信息
-        public string Error { get; set; }
-    }
-
+    //
+    // 摘要:
+    //     获得/设置 文件名
+    public string FileName { get; set; }
+    //
+    // 摘要:
+    //     获得/设置 原始文件名
+    public string OriginFileName { get; }
+    //
+    // 摘要:
+    //     获得/设置 文件大小
+    public long Size { get; set; }
+    //
+    // 摘要:
+    //     获得/设置 文件上传结果 0 表示成功 非零表示失败
+    public int Code { get; set; }
+    //
+    // 摘要:
+    //     获得/设置 文件预览地址
+    public string PrevUrl { get; set; }
+    //
+    // 摘要:
+    //     获得/设置 错误信息
+    public string Error { get; set; }
 }
