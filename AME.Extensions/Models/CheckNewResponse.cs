c1cb3cbc9b97796f41de4b006269b1ba09f60ade
Alex chow <zhouchuanglin@gmail.com>
2024-06-25 09:49:05 +0200

    Update CheckNewResponse.cs

--- AME.Extensions/Models/CheckNewResponse.cs
+++ AME.Extensions/Models/CheckNewResponse.cs
@@ -4,31 +4,28 @@
 // e-mail:zhouchuanglin@gmail.com 
 // **********************************
 
-namespace AME.Models
+namespace AME.Models;
+
+
+public class CheckNewResponse
 {
+    /// <summary>
+    /// 文件名,带相对路径
+    /// </summary>
+    public string Filename { get; set; }
 
-    public class CheckNewResponse
-    {
-        /// <summary>
-        /// 文件名,带相对路径
-        /// </summary>
-        public string Filename { get; set; }
+    /// <summary>
+    /// 文件大小
+    /// </summary>
+    public string Size { get; set; }
 
-        /// <summary>
-        /// 文件大小
-        /// </summary>
-        public string Size { get; set; }
+    /// <summary>
+    /// 建立时间
+    /// </summary>
+    public string CreationTime { get; set; }
 
-        /// <summary>
-        /// 建立时间
-        /// </summary>
-        public string CreationTime { get; set; }
-
-        /// <summary>
-        /// 刷新时间
-        /// </summary>
-        public string LasFetch { get; set; }
-    }
-
-
+    /// <summary>
+    /// 刷新时间
+    /// </summary>
+    public string LasFetch { get; set; }
 }
