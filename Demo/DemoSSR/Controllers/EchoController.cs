f57cf7ab7e202c35c2f2fabc6929c790101334c2
Alex chow <zhouchuanglin@gmail.com>
2025-01-22 12:33:27 +0100

    EchoController

---
+++ Demo/DemoSSR/Controllers/EchoController.cs
@@ -0,0 +1,36 @@
+﻿// ********************************** 
+// Densen Informatica 中讯科技 
+// 作者：Alex Chow
+// e-mail:zhouchuanglin@gmail.com 
+// **********************************
+
+using BootstrapBlazor.AzureServices;
+using BootstrapBlazor.Ocr.Services;
+using BootstrapBlazor.OpenAI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AME.Controllers;
+#nullable enable 
+ 
+[ApiController]
+[Route("[controller]")]
+public class EchoController : ControllerBase
+{
+    /// <summary>
+    /// Echo接口
+    /// </summary>
+    /// <param name="info"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [HttpGet]
+    public IActionResult Echo(string info = "test")
+    {
+        if (string.IsNullOrWhiteSpace(info))
+        {
+            return BadRequest("输入不能为空");
+        }
+
+        return Ok(info);
+    }
+
+}
