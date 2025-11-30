7b46e2391dfcfdae73c1ab2b959ba7ee59220c49
Alex chow <zhouchuanglin@gmail.com>
2025-11-30 16:03:19 +0100

    Update ObjectExtensions.cs

--- AME.Extensions/Extensions/ObjectExtensions.cs
+++ AME.Extensions/Extensions/ObjectExtensions.cs
@@ -35,13 +35,13 @@ public static partial class ObjectExtensions
             var type = item.GetType();
             if (typeof(ICloneable).IsAssignableFrom(type))
             {
-                var clv = type.GetMethod("Clone")?.Invoke(type, null);
+                var clv = type.GetMethod("Clone")?.Invoke(item, null);
                 if (clv != null)
                 {
                     ret = (TItem)clv;
                 }
             }
-            if (type.IsClass)
+            else if (type.IsClass)
             {
                 ret = Activator.CreateInstance<TItem>();
                 var valType = ret?.GetType();
